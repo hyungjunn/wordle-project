7914d5f9cdb785634f9dc6cce7258fc331dc8a5a
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-09-29 11:01:23 +0900

    impl addLetter

---
+++ src/js/main.js
@@ -0,0 +1,35 @@
+const letters = document.querySelectorAll(".scoreboard-letter");
+const ANSWER_LENGTH = 5;
+
+async function init() {
+  let currentGuess = "";
+  function addLetter(letter) {
+    if (currentGuess.length < ANSWER_LENGTH) {
+      currentGuess += letter;
+    } else {
+      currentGuess =
+        currentGuess.substring(0, currentGuess.length - 1) + letter;
+    }
+
+    letters[currentGuess.length - 1].innerText = letter;
+  }
+  document.addEventListener("keydown", function handleKeyPress(event) {
+    const action = event.key;
+
+    if (action === "Enter") {
+      commit();
+    } else if (action === "Backspace") {
+      backspace();
+    } else if (isLetter(action)) {
+      addLetter(action.toUpperCase());
+    } else {
+      // do nothing
+    }
+  });
+}
+
+function isLetter(letter) {
+  return /^[a-zA-Z]$/.test(letter);
+}
+
+init();
