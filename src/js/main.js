c53517c132176850971e263443c490f64e6146c1
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-10-12 12:01:37 +0900

    implement wordle game

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,156 @@
-const letters = document.querySelectorAll(".scoreboard-letter");
 const ANSWER_LENGTH = 5;
+const ROUNDS = 6;
 
-async function init() {
-  let currentGuess = "";
-  function addLetter(letter) {
-    if (currentGuess.length < ANSWER_LENGTH) {
-      currentGuess += letter;
-    } else {
-      currentGuess =
-        currentGuess.substring(0, currentGuess.length - 1) + letter;
-    }
+const letters = document.querySelectorAll(".scoreboard-letter");
+const loadingDiv = document.querySelector(".info-bar");
 
-    letters[currentGuess.length - 1].innerText = letter;
+let expectedWord = "";
+let currentRow = 0;
+let answer;
+
+const commit = async function () {
+  if (ANSWER_LENGTH !== expectedWord.length) return;
+  await validateWord(expectedWord);
+};
+
+const getAnswer = async function () {
+  setLoading(false);
+  const url = "https://words.dev-apis.com/word-of-the-day";
+  try {
+    const response = await fetch(url);
+    const { word } = await response.json();
+    answer = word.toUpperCase();
+    console.log(answer);
+  } catch (err) {
+    console.error(err);
   }
-  document.addEventListener("keydown", function handleKeyPress(event) {
-    const action = event.key;
+};
 
-    if (action === "Enter") {
-      commit();
-    } else if (action === "Backspace") {
-      backspace();
-    } else if (isLetter(action)) {
-      addLetter(action.toUpperCase());
-    } else {
-      // do nothing
-    }
+const validateWord = async function (word) {
+  setLoading(true);
+
+  const url = "https://words.dev-apis.com/validate-word";
+  const response = await fetch(url, {
+    method: "POST",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify({ word: `${word}` }),
   });
-}
+  if (!response.ok) {
+    throw new Error(`Response status: ${response.status}`);
+  }
+  const { validWord } = await response.json();
 
-function isLetter(letter) {
+  console.log(validWord);
+
+  setLoading(false);
+
+  // 다 틀린 경우
+  if (!validWord) {
+    markInvalid();
+    return;
+  }
+
+  let allRight = true;
+
+  const expectedParts = expectedWord.split("");
+  const answerParts = answer.split("");
+  const map = makeMap(answerParts);
+  // 맞춘 경우
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (expectedParts[i] === answerParts[i]) {
+      letters[i + ANSWER_LENGTH * currentRow].classList.add("correct");
+      map[expectedParts[i]]--;
+    }
+  }
+
+  // 근접한 경우
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    if (expectedParts[i] === answerParts[i]) {
+      // do nothing
+    } else if (
+      answerParts.includes(expectedParts[i]) &&
+      map[expectedParts[i]] >= 1
+    ) {
+      allRight = false;
+      letters[i + ANSWER_LENGTH * currentRow].classList.add("close");
+      map[expectedParts[i]]--;
+    } else {
+      allRight = false;
+      letters[i + ANSWER_LENGTH * currentRow].classList.add("wrong");
+    }
+  }
+
+  currentRow++;
+  expectedWord = "";
+  if (allRight) {
+    document.querySelector(".brand").classList.add("winner");
+    alert("You win!!!");
+  } else if (ROUNDS === currentRow) {
+    alert(`You are lose. The Answer is ${answer}`);
+  }
+};
+
+const setLoading = function (isLoading) {
+  loadingDiv.classList.toggle("show", isLoading);
+};
+
+const isLetter = function (letter) {
   return /^[a-zA-Z]$/.test(letter);
-}
+};
+
+const markInvalid = function () {
+  for (let i = 0; i < ANSWER_LENGTH; i++) {
+    letters[i + ANSWER_LENGTH * currentRow].classList.add("invalid");
+  }
+  currentRow++;
+  expectedWord = "";
+};
+
+const makeMap = function (array) {
+  const obj = {};
+  for (let i = 0; i < array.length; i++) {
+    if (obj[array[i]]) {
+      obj[array[i]]++;
+    } else {
+      obj[array[i]] = 1;
+    }
+  }
+  return obj;
+};
+
+const backspace = function () {
+  if (expectedWord === "") return;
+  letters[expectedWord.length + ANSWER_LENGTH * currentRow - 1].innerText = "";
+  expectedWord = expectedWord.slice(0, expectedWord.length - 1);
+  expectedWord.length--;
+};
+
+const keyRender = function (letter) {
+  if (expectedWord.length < ANSWER_LENGTH) {
+    expectedWord += letter;
+  } else {
+    expectedWord = expectedWord.slice(0, expectedWord.length - 1) + letter;
+  }
+  letters[expectedWord.length + ANSWER_LENGTH * currentRow - 1].innerText =
+    letter;
+};
+
+const init = async function () {
+  await getAnswer();
+  const pressKey = function () {
+    document.addEventListener("keydown", (event) => {
+      if (event.key === "Backspace") {
+        backspace();
+      } else if (event.key === "Enter") {
+        commit();
+      } else if (isLetter(event.key)) {
+        keyRender(event.key.toUpperCase());
+      }
+      // do nothing
+      return;
+    });
+  };
+  pressKey();
+};
 
 init();
